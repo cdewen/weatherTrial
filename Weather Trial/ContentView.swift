52f6bd4fc3247e4663433821a489c522bb373754
cdewen <96959326+cdewen@users.noreply.github.com>
2023-03-14 16:21:42 -0500

    Initial Commit

---
+++ Weather Trial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather Trial
+//
+//  Created by Carter Ewen on 3/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather Trial/Weather_TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TrialApp.swift
+//  Weather Trial
+//
+//  Created by Carter Ewen on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
